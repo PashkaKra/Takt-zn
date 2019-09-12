edd9e1b624078ba060fbde50f58210282055962f
PashkaKra <pav892@yandex.ru>
2019-09-12 06:56:16 +0300

    first commit

---
+++ JavaScript/Links.js
@@ -0,0 +1,6 @@
+function Index(){
+document.getElementById('header').innerHTML = 'Основные условные обозначения применяемые в боевых документах';
+document.getElementById('str1').innerHTML = '<td><a class="knopka-" href="HTML/1.Obuchenie.html">Обучение</a></td>';
+document.getElementById('str2').innerHTML = '<td><a class="knopka-" href="HTML/2.Kontrol_znaniy.html">Контроль знаний</a></td>';
+document.getElementById('str3').innerHTML = '<td><a class="knopka-" href="HTML/3.Ekzamen.html">Экзамен</a></td>';
+}
---
+++ JavaScript/kontrl.js
@@ -0,0 +1,59 @@
+function vkltrue()
+{document.getElementById("vkl-true").style.display="block";
+document.getElementById("ex").style.display="block";
+document.getElementById("exv").style.display="block";
+document.getElementById("img").style.display="block";
+}
+
+function vklfalse1()
+{document.getElementById("vkl-false").style.display="block";
+document.getElementById("fal1").style.display="block";
+document.getElementById("exi").style.display="block";
+document.getElementById("exiv").style.display="block";
+document.getElementById("img").style.display="block";
+}
+
+function vklfalse2()
+{document.getElementById("vkl-false").style.display="block";
+document.getElementById("fal2").style.display="block";
+document.getElementById("exi").style.display="block";
+document.getElementById("exiv").style.display="block";
+document.getElementById("img").style.display="block";
+}
+
+function vklfalse3()
+{document.getElementById("vkl-false").style.display="block";
+document.getElementById("fal3").style.display="block";
+document.getElementById("exi").style.display="block";
+document.getElementById("exiv").style.display="block";
+document.getElementById("img").style.display="block";
+}
+
+function exit()
+{document.getElementById("vkl-true").style.display="none";
+/*document.getElementById("ex").style.display="none";*/
+document.getElementById("vkl-false").style.display="none";
+document.getElementById("fal1").style.display="none";
+document.getElementById("fal2").style.display="none";
+document.getElementById("fal3").style.display="none";
+document.getElementById("img").style.display="none";
+/*document.getElementById("exi").style.display="none";*/
+}
+
+/*function zakr1()
+{document.getElementById("vkl-false").style.display="none";
+document.getElementById("fal1").style.display="none";
+document.getElementById("exi").style.display="none";
+}
+
+function zakr2()
+{document.getElementById("vkl-false").style.display="none";
+document.getElementById("fal2").style.display="none";
+document.getElementById("exi").style.display="none";
+}
+
+function zakr3()
+{document.getElementById("vkl-false").style.display="none";
+document.getElementById("fal3").style.display="none";
+document.getElementById("exi").style.display="none";
+}*/
